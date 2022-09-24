638bbf177215a692d3cd34858ed4213a3a82ef6f
Ewerton Silveira <ewerton@farmote.com>
2022-09-25 00:07:15 +1200

    initial files

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+from states.menu import Menu
+from states.gameplay import Gameplay
+from states.game_over import GameOver
+from states.splash import Splash
+from game import Game
+from collections import namedtuple
+
+Size = namedtuple('Size',"width height")
+ScreenSize = Size(1920, 1080)
+
+pygame.init()
+screen = pygame.display.set_mode(ScreenSize)
+pygame.display.set_caption('Kylie Jumper')
+
+states = {
+    "MENU": Menu(),
+    "SPLASH": Splash(),
+    "GAMEPLAY": Gameplay(),
+    "GAME_OVER": GameOver(),
+}
+
+game = Game(screen, states, "SPLASH")
+game.run()
+
+pygame.quit()
+sys.exit()
