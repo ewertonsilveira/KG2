b97ca3eae50631f6e2d5015b21c4dd4e36711c7b
Ewerton Silveira <ewerton@farmote.com>
2022-09-30 13:42:34 +1300

    jump

--- states/shooter.py
+++ states/shooter.py
@@ -1,14 +1,40 @@
 import pygame
 from .base import BaseState
 
-class Soldier(pygame.sprite.Sprite):
-    def __init__(self, x, y, scale, speed):
+# define game variables
+GRAVITY = 0.75
+COOLDOWN_PERIOD = 90
+class Character(pygame.sprite.Sprite):
+    def __init__(self, chart_type, x, y, scale, speed):
         pygame.sprite.Sprite.__init__(self)
         # Variables
+        self.alive = True
+        self.char_type = chart_type
         self.speed = speed
+        self.flip = False
+        self.direction = 1
+        self.vel_y = 0
+        self.animation_list = []
+        self.frame_index = 0
+        self.action = 0
+        self.jump = False
+        self.image_update_time = pygame.time.get_ticks()
 
-        img = pygame.image.load('public/graphics/player/idle/0.png').convert_alpha()
-        self.image = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
+        tmp_list = []
+        for i in range(5):
+            img = pygame.image.load(f'public/graphics/{self.char_type}/idle/{i}.png').convert_alpha()
+            img = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale))) 
+            tmp_list.append(img)
+        self.animation_list.append(tmp_list)
+
+        tmp_list = []
+        for i in range(6):
+            img = pygame.image.load(f'public/graphics/{self.char_type}/run/{i}.png').convert_alpha()
+            img = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
+            tmp_list.append(img)
+        self.animation_list.append(tmp_list)
+
+        self.image = self.animation_list[self.action][self.frame_index]
         self.rect = img.get_rect()
         self.rect.center = (x, y)
     
@@ -16,19 +42,48 @@ class Soldier(pygame.sprite.Sprite):
         # reset movement variables
         dx = 0
         dy = 0
-
         # assign movement variables if moving left or right
         if moving_left:            
             dx = -self.speed
+            self.flip = True
+            self.direction = -1
         if moving_right:
             dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        # jump
+        if self.jump:
+            self.vel_y = -11
+
+        dy += self.vel_y
 
         # update rectangle pos
         self.rect.x += dx
         self.rect.y += dy
+    
+    def update_action(self, new_action):
+        # update only if is different one
+        if self.action != new_action:
+            self.action = new_action
+            # update the animation settings
+            self.frame_index = 0
+            self.image_update_time = pygame.time.get_ticks()
+
+    def update_animation(self):
+        # update new image
+        self.image = self.animation_list[self.action][self.frame_index]
+        # check if enough fime has passed
+        if pygame.time.get_ticks() - self.image_update_time > COOLDOWN_PERIOD:
+            self.image_update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+        
+        # if animation has run out of images, reset back to initial
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
 
     def draw(self, surface):
-        surface.blit(self.image, self.rect)
+        surface.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
 
 
 class Shooter(BaseState):
@@ -37,21 +92,32 @@ class Shooter(BaseState):
         self.next_state = "MENU"
         self.bgColor = (144,201,120)
 
+        # player action variables
         self.moving_left = False
         self.moving_right = False
 
-        print(self.moving_left)
-        print(self.moving_right)
-
-        self.players = [Soldier(200, 200, 3, 5)]
+        self.player = Character("player", 200, 200, 3, 5)
+        self.enemies = [Character("enemy", 300, 200, 3, 5)]
 
 
     def draw(self, surface):
         self.draw_bg(surface, self.bgColor)
 
-        for _, player in enumerate(self.players):
-            player.draw(surface)
-            player.move(self.moving_left, self.moving_right)
+        for _, enemy in enumerate(self.enemies):
+            enemy.draw(surface)
+            
+          
+        self.player.draw(surface)
+        self.player.update_animation()
+
+        # update player action
+        if self.player.alive:
+            if self.moving_left or self.moving_right:
+                self.player.update_action(1) #1 run
+            else:
+                self.player.update_action(0) #1 idle
+
+            self.player.move(self.moving_left, self.moving_right)
         
 
     def get_event(self, event):
@@ -63,6 +129,8 @@ class Shooter(BaseState):
                 self.moving_left = True
             elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
                 self.moving_right = True
+            elif (event.key == pygame.K_w or event.key == pygame.K_UP) and self.player.alive:
+                self.jump = True
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_a or event.key == pygame.K_LEFT:
