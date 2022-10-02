7b9026edd24acbc41c91a2c2701915bb808dbbf2
Ewerton Silveira <ewerton@farmote.com>
2022-10-02 17:56:48 +1300

    explosion class

--- states/shooting/grenade.py
+++ states/shooting/grenade.py
@@ -2,7 +2,8 @@ from email.headerregistry import Group
 import pygame
 import os
 
-from states.settings import * 
+from states.settings import *
+from states.shooting.explosion import Explosion 
 
 class Grenade(pygame.sprite.Sprite):
     def __init__(self, surface, x, y, direction):
@@ -11,11 +12,12 @@ class Grenade(pygame.sprite.Sprite):
         self.bullet_image = None
         self.vel_y = -11
         self.speed = GRENADE_SPEED
-        self.timer = GRENADE_TIMER,
+        self.timer = GRENADE_TIMER
         self.direction = direction
         self.image = self.get_image()
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
+        self.grenade_explode_group = pygame.sprite.Group()
 
     def update(self):
         # gravity affecting the grenade
@@ -35,8 +37,16 @@ class Grenade(pygame.sprite.Sprite):
 
         self.rect.y +=dy
         self.rect.x += dx
-    
 
+        self.timer -= 1
+        if self.timer <= 0:
+            explosion = Explosion(self.surface, self.rect.x, self.rect.y, GRENADE_EXPLOSION_SCALE)
+            self.grenade_explode_group.add(explosion)
+            self.kill()
+    
+        self.grenade_explode_group.update()
+        self.grenade_explode_group.draw(self.surface)
+    
     def get_image(self):
         if self.bullet_image == None:        
             self.bullet_image = pygame.image.load("public/graphics/icons/grenade.png").convert_alpha();
