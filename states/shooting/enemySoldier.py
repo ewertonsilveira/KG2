c69c115ca3c0ee404315de7e5709ab8a1feeee61
Ewerton Silveira <ewerton@farmote.com>
2022-10-06 15:53:41 +1300

    alive

--- states/shooting/enemySoldier.py
+++ states/shooting/enemySoldier.py
@@ -19,38 +19,37 @@ class EnemySoldier(Soldier):
 
     def ai(self, surface, target):
         if not self.alive or not target.alive:
-            self.update_action(0) # idle
+            # self.update_action(0) # idle
             return
+        
+        if self.idling == False and random.randint(1, 300) == 1:
+            self.idling = True  
+            self.idling_counter = 50
+            self.update_action(0) # idle
 
-        if self.alive and target.alive:
-            if self.idling == False and random.randint(1, 300) == 1:
-                self.idling = True  
-                self.idling_counter = 50
-                self.update_action(0) # idle
+        # shoot if player is in the range
+        if self.vision.colliderect(target.rect):
+            self.update_action(0) # idle
+            self.shoot(surface)
+        else:
+            if self.idling == False:
+                ai_move_right = True if self.direction == 1 else False
+                ai_move_left = not ai_move_right
+                self.move(ai_move_left, ai_move_right)
+                self.update_action(1) # run
+                self.move_counter += 1
 
-            # shoot if player is in the range
-            if self.vision.colliderect(target.rect):
-                self.update_action(0) # idle
-                self.shoot(surface)
-            else:
-                if self.idling == False:
-                    ai_move_right = True if self.direction == 1 else False
-                    ai_move_left = not ai_move_right
-                    self.move(ai_move_left, ai_move_right)
-                    self.update_action(1) # run
-                    self.move_counter += 1
+                #update ai vision as the enemy moves
+                self.vision.center = (self.rect.centerx + 75 * self.direction, self.rect.centery)
 
-                    #update ai vision as the enemy moves
-                    self.vision.center = (self.rect.centerx + 75 * self.direction, self.rect.centery)
-
-                    # move direction
-                    if self.move_counter > TILE_SIZE:
-                        self.direction *= -1
-                        self.move_counter *= -1
-                else:                
-                    self.idling_counter -= 1
-                    if self.idling_counter <= 0:
-                        self.idling = False
+                # move direction
+                if self.move_counter > TILE_SIZE:
+                    self.direction *= -1
+                    self.move_counter *= -1
+            else:                
+                self.idling_counter -= 1
+                if self.idling_counter <= 0:
+                    self.idling = False
 
 
     def throw_grenade(self, surface):
